ff0c60fcb715802aaa9cfeab8e9959abd7f1a565
IAmJaysWay <91806278+IAmJaysWay@users.noreply.github.com>
2023-03-24 17:26:41 +0200

    first commit

---
+++ Approval/backend/index.js
@@ -0,0 +1,49 @@
+/* const Moralis = require("moralis").default; */
+const express = require("express");
+const app = express();
+const port = 3001;
+const cors = require("cors");
+const axios = require("axios");
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/approvals", async (req, res) => {
+  const { chain, wallet } = req.query;
+
+  const options = {
+    method: "GET",
+    headers: {
+      "accept": "application/json",
+      "X-API-Key": "KVuq1YrCBfFSjtlFDHB2zEhEuvIxsU92aMO4rkW7QJj9zkFa9JQbEDwmlKjcilly",
+    }
+  }
+  const walletAddresses = [wallet];
+
+  const response = await fetch(
+    `https://deep-index.moralis.io/api/v2/erc20/approvals?chain=${chain}&wallet_addresses=${walletAddresses}`,
+    options
+  );
+
+  if (response.ok){
+    const data = await response.json();
+
+    data.result.forEach((e,i)=>{
+      e["key"] = i +1;
+    })
+
+    return res.status(200).json(data.result);
+  }else {
+    return res.status(400).json();
+  }
+
+
+  
+});
+
+
+
+  app.listen(port, () => {
+    console.log(`Listening to API Calls`);
+  });
---
+++ Approval/frontend/src/App.js
@@ -0,0 +1,134 @@
+import "./App.css";
+import { useState } from "react";
+import logo from "./moralisLogo.svg";
+import { Table, Input, Select, Spin, message } from "antd";
+import axios from "axios";
+
+const { Search } = Input;
+function App() {
+  const [messageApi, contextHolder] = message.useMessage();
+  const [walletAddress, setWalletAddress] = useState(null);
+  const [searching, setSearching] = useState(false);
+  const [chain, setChain] = useState("eth");
+  const [dataSource, setDataSource] = useState(null);
+
+  const columns = [
+    {
+      title: "Tx Hash",
+      dataIndex: "transaction_hash",
+      key: "transaction_hash",
+      render: (text) =><a href={`https://etherscan.io/tx/${text}`} target="_blank" rel="noreferrer">{text.slice(0,4)}...{text.slice(-4)}</a>
+    },
+    {
+      title: "Updated",
+      dataIndex: "block_timestamp",
+      key: "block_timestamp",
+      render: (text) =><div>{text.slice(0,10)}</div>
+    },
+    {
+      title: "Asset",
+      dataIndex: "contract_address",
+      key: "contract_address",
+      render: (text) =><a href={`https://etherscan.io/address/${text}`} target="_blank" rel="noreferrer">{text.slice(0,4)}...{text.slice(-4)}</a>
+    },
+    {
+      title: "Approved Spender",
+      dataIndex: "to_wallet",
+      key: "to_wallet",
+      render: (text) =><a href={`https://etherscan.io/address/${text}`} target="_blank" rel="noreferrer">{text.slice(0,4)}...{text.slice(-4)}</a>
+    },
+    {
+      title: "Allowance",
+      dataIndex: "value",
+      key: "value",
+      render: (text) => <>
+        {text.length > 24 ? <div>Infinite</div>:<div>{Number(text)/1e18}</div>}</>
+      
+    },
+    {
+      title: "Possible Spam",
+      dataIndex: "possible_spam",
+      key: "possible_spam",
+      render: (text) => <>
+      {text ? <div>!!</div> : <div>-</div>}
+      </>
+    }
+  ];
+
+  const chains = [
+    {
+      value: "eth",
+      label: "Ethereum",
+    },
+    {
+      value: "polygon",
+      label: "Polygon",
+    },
+  ];
+
+  async function onSearch() {
+    setSearching(true);
+
+    let res;
+    try{
+    res = await axios.get(`http://localhost:3001/approvals`, {
+      params: { chain: chain, wallet: walletAddress },
+    });
+    }catch(error){
+      messageApi.open({
+        type: 'warning',
+        content: 'Unsupported request...',
+      });
+      setSearching(false);
+      return
+    }
+
+    setDataSource(res.data);
+    setSearching(false);
+  }
+
+  return (
+    <>
+    {contextHolder}
+    <div className="App">
+      <img src={logo} alt={"moralis"} width={100} />
+      <h1>ERC-20 Token Approvals (Demo)</h1>
+
+      <div className="resolutions">
+        <div className="searchOptions">
+          {chains && (
+            <Select
+              defaultValue="eth"
+              options={chains}
+              onChange={(val) => {
+                setChain(val);
+                setDataSource(null);
+              }}
+            />
+          )}
+          <Search
+            className="search"
+            value={walletAddress}
+            onChange={(e) => {
+              setWalletAddress(e.target.value);
+              setDataSource(null);
+            }}
+            placeholder="Wallet Address..."
+            enterButton="Search"
+            size="large"
+            onSearch={onSearch}
+          />
+        </div>
+        {dataSource && !searching && (
+          <Table className="table" dataSource={dataSource} columns={columns} />
+        )}
+        {searching && 
+        <Spin />
+        }
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
